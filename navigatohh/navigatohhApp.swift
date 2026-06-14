35119c6fc6181f25d06c9eaad302e26fb89d4c26
Temiloluwa Akisanya <tolutech2004@gmail.com>
2026-06-14 16:04:58 +0100

    Initial Commit

---
+++ navigatohh/navigatohhApp.swift
@@ -0,0 +1,17 @@
+//
+//  navigatohhApp.swift
+//  navigatohh
+//
+//  Created by Temiloluwa Akisanya on 14/06/2026.
+//
+
+import SwiftUI
+
+@main
+struct navigatohhApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
